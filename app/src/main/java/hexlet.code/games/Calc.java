316b6392b57f22e944af1d7c2e5f5bbb6b760e56
Алексей Бельков <orlais@yandex.ru>
2022-10-22 23:15:07 +0300

    Игра: "Калькулятор"

--- app/src/main/java/hexlet.code/games/Calc.java
+++ app/src/main/java/hexlet.code/games/Calc.java
@@ -6,21 +6,21 @@ public class Calc {
     }
 
     public static String question() {
-    // Генерируем первое число
-    int firstNumber = (int) (Math.random() * 100);
-    // Генерируем следующее число
-    int secondNumber = (int) (Math.random() * 100);
-    // Генерируем производимую операцию
-    int operationNumber = (int) (Math.random() * 3);
-    String question;
-    if (operationNumber == 0) {
-        question = firstNumber + " + " + secondNumber;
-    } else if (operationNumber == 1) {
-        question = firstNumber + " - " + secondNumber;
-    } else {
-        question = firstNumber + " * " + secondNumber;
-    }
-    return question;
+        // Генерируем первое число
+        int firstNumber = (int) (Math.random() * 100);
+        // Генерируем следующее число
+        int secondNumber = (int) (Math.random() * 100);
+        // Генерируем производимую операцию
+        int operationNumber = (int) (Math.random() * 3);
+        String question;
+        if (operationNumber == 0) {
+            question = firstNumber + " + " + secondNumber;
+        } else if (operationNumber == 1) {
+            question = firstNumber + " - " + secondNumber;
+        } else {
+            question = firstNumber + " * " + secondNumber;
+        }
+        return question;
     }
 
     public static String correctAnswer(String question) {
